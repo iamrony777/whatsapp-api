ed1e81565204fbc876b2cf2e6e128a6d156e707e
Saurabh G <34957562+IAmBlackat@users.noreply.github.com>
2023-11-12 20:42:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const axios = require('axios');
 const fs = require('fs');
 const pino = require('pino');
 const { DisconnectReason, MessageType, MessageOptions, Mimetype } = require("@whiskeysockets/baileys");
-const mongoURL = process.env.MONGO_URL || "mongodb+srv://flyingmindsagency:EuusPWxAmmUdSrvb@cluster0.vzfuc2h.mongodb.net";
+const mongoURL = process.env.MONGO_URL || "mongodb+srv://flyingmindsagency:7w2GeXD43ujyYlQQ@cluster0.1d8tp7o.mongodb.net";
 const { makeMongoStore } = require("@iamrony777/baileys");
 
 app.use(express.json());
